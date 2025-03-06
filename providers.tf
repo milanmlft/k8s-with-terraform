cbb3b8cc411d03aa6f526b5643e65c75cf3a2e83
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-03-06 14:04:51 +0000

    Add helm provider

--- providers.tf
+++ providers.tf
@@ -4,10 +4,26 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.11.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
   }
+  required_version = "1.5.7"
+}
+
+locals {
+  kubeconfig = "~/.kube/config"
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
+  config_path    = local.kubeconfig
   config_context = "minikube"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = local.kubeconfig
+  }
+
 }
